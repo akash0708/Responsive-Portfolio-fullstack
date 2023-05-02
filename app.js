7eb8715b22d17d9933564665c2992e2cfb344f27
Akash <akashbag12a05@gmail.com>
2023-05-02 11:37:59 +0530

    backend updated

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add('active-link');
-    document.getElementById(tabname).classList.add('active-tab')
-}
+const app = express();
+app.use(express.json());
 
-var sidemenu = document.getElementById('sidemenu');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-function openmenu(){
-    sidemenu.style.right = "0";
-}
-function closemenu(){
-    sidemenu.style.right = "-200px";
-}
+mongoose.connect('mongodb+srv://akashbag12a05:myieeeapp1@skycluster1.ip9s82v.mongodb.net/formsDB', {useNewUrlParser: true});
+
+const Message = mongoose.model('Message', {
+    name: String,
+    email: String,
+    message: String
+  });
+
+  app.get("/", function(req, res){
+    res.sendFile(__dirname+"/index.html");
+  });
+
+  app.post('/submit', (req, res) => {
+    const message = new Message({
+      name: req.body.name,
+      email: req.body.email,
+      message: req.body.message
+    });
+    
+    message.save()
+      .then(() => {
+        res.status(200).send('Message received');
+      })
+      .catch((err) => {
+        res.status(500).send('Error saving message');
+      });
+  });
+
+  app.listen(3000, () => {
+    console.log('Server running on port 3000');
+  });
+
+//   myieeeapp1
